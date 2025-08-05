c8a4f58b566305471af78f508e04fd34204d9b31
Виолетта <violetta.karavay@gmail.com>
2025-08-05 15:54:04 +0300

    Пройден урок 4

---
+++ Python_Lessens/Lessens_4-.py
@@ -0,0 +1,11 @@
+m = int(input("Введите первое число: "))
+
+n = int(input('Введите второе число: '))
+
+print(f"Результат {n + m}")
+print(f"Результат {n - m}")
+print(f"Результат {pow(n, m)}")
+print(f"Результат {n // m}")
+
+word = 'Z-z-'
+print(word * 10, end= 'Z\n')
